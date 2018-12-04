40c735d5c7c6fc67055495112c3571d16229d512
JiaxuanYue2 <jiaxuan199531@gmail.com>
2018-12-04 21:47:46 +0000

    Finish layout and routes

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.default')
+
 @section('content')
-  <h1>主页</h1>
+  <div class="jumbotron">
+    <h1>随便取名</h1>
+    <p class="lead">
+      点击 <a href="https://www.baidu.com">百度</a> 你的问题。
+    </p>
+    <p>
+      唉
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="{{route('signup')}}" role="button">注册用</a>
+    </p>
+  </div>
 @stop
